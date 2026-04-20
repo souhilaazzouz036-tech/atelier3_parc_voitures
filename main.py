7ac3f61c3a4005a6658d4fcb5e2461553c93d21f
souhila <souhila.azzouz@monboreal.ca>
2026-04-20 00:52:48 -0400

    Ajout de la sortie d'une voiture du parc

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from voiture import Voiture
+from parc import parc
 
+parc = parc(1, "toronto", 3)
+
+v1 = Voiture("A23", "toyota", "rouge")
+v2 = Voiture("B256", "BMW", "BLANC")
+v3 = Voiture("C789", "HONDA", "NOIR")
+
+parc.entrer_une_voiture(v1)
+parc.entrer_une_voiture(v2)
+parc.entrer_une_voiture(v3)
 
 
 
