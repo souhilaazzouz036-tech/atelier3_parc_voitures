577edfe4f4073fd74f610cdc7121daa7945a4fda
souhila <souhila.azzouz@monboreal.ca>
2026-04-20 00:56:35 -0400

    Ajout de la méthode sortir_une_voiture

--- parc.py
+++ parc.py
@@ -4,3 +4,14 @@ class parc:
         self.adresse = adresse
         self.capacite = capacite
         self.listevoitures = []
+
+    def entrer_une_voiture(self, voiture):
+        if voiture in self.listevoitures:
+            print("la voiture existe deja dans le parc")
+            return
+        if len(self.listevoitures) >= self.capacite:
+            print("parc est plein")
+            return
+        self.listevoitures.append(voiture)
+        print("voiture ajoutee avec succes")
+
