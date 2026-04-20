b7ba90e92ed41fab5ec10ed34b6764c8d0a59d42
souhila <souhila.azzouz@monboreal.ca>
2026-04-20 00:55:50 -0400

    Ajout de la méthode entrer_une_voiture

--- parc.py
+++ parc.py
@@ -1,1 +1,6 @@
-
+class parc:
+    def __init__(self, id, adresse, capacite):
+        self.id = id
+        self.adresse = adresse
+        self.capacite = capacite
+        self.listevoitures = []
